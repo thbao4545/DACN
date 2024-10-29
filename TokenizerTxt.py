325099c78d7fc73f1cedfa71a752091e92dc878a
unknown <toan.tinh1604@hcmt.edu.vn>
2024-10-29 13:34:43 +0700

    voice rec added

--- TokenizerTxt.py
+++ TokenizerTxt.py
@@ -1,8 +1,9 @@
 # import the existing word and sentence tokenizing  
 # libraries 
 from nltk.tokenize import sent_tokenize, word_tokenize 
-  
-text = "Đường Nguyễn Thái Bình đang kẹt xe"
-  
+
+text = "Ngã tư Thủ Đức trên đường Xa lộ Hà Nội đang kẹt xe!"
+
 print(sent_tokenize(text)) 
+
 print(word_tokenize(text)) 
